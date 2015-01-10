9e3f43dedcbdcd0b8a3b40507c634e797e40caa2
Ulises Cardenas <ulises.card@gmail.com>
2015-01-10 17:37:05 -0600

    4
    4

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var mongoose = require('./data/db');
+
 var app = express();
 
 // view engine setup
---
+++ models/trailers.js
@@ -0,0 +1,9 @@
+var trailerSchema = new mongoose.Schema({
+  name: String
+});
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('Trailer', trailerSchema);
+
+
+
