b2fc86127e51739348a3851297fb33cf01fadaeb
Ulises Cardenas <ulises.card@gmail.com>
2015-01-10 18:11:55 -0600

    3
    3

--- app.js
+++ app.js
@@ -14,10 +14,7 @@ var mongoose = require ("mongoose"); // The reason for this demo.
 
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.
-var uristring =
-process.env.MONGOLAB_URI ||
-process.env.MONGOHQ_URL ||
-'mongodb://localhost/HelloMongoose';
+var uristring = process.env.MONGOLAB_URI;
 
 // The http server will listen to an appropriate port, or default to
 // port 5000.
